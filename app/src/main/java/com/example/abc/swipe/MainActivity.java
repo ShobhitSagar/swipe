0b2e7917a427438846ec9e3d8dfeef981d8fd16a
ShobhitSagar <shobhitsagar109@gmail.com>
2017-08-20 17:42:49 +0530

    Made some changes

--- app/src/main/java/com/example/abc/swipe/MainActivity.java
+++ app/src/main/java/com/example/abc/swipe/MainActivity.java
@@ -2,77 +2,81 @@ package com.example.abc.swipe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 import android.view.MotionEvent;
 import android.view.GestureDetector;
 import android.support.v4.view.GestureDetectorCompat;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener,
 GestureDetector.OnDoubleTapListener {
 
-    private TextView vipsmessage;
     private GestureDetectorCompat gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        vipsmessage =(TextView)findViewById(R.id.vipsmessage);
+
         this.gestureDetector = new GestureDetectorCompat(this,this);
         gestureDetector.setOnDoubleTapListener(this);
     }
 
     @Override
+    public boolean onTouchEvent(MotionEvent e) {
+        return gestureDetector.onTouchEvent(e);
+    }
+
+    @Override
     public boolean onSingleTapConfirmed(MotionEvent e) {
-        vipsmessage.setText("onSingleTapConfirmed");
+        Toast.makeText(getApplicationContext(), "onSingleTapConfirmed", Toast.LENGTH_SHORT).show();
         return true;
     }
 
     @Override
     public boolean onDoubleTap(MotionEvent e) {
-        vipsmessage.setText("onDoubleTap");
+        Toast.makeText(getApplicationContext(), "onDoubleTap", Toast.LENGTH_SHORT).show();
         return true;
     }
 
     @Override
     public boolean onDoubleTapEvent(MotionEvent e) {
-        vipsmessage.setText("onDoubleTapEvent");
-        return false;
+        Toast.makeText(getApplicationContext(), "onDoubleTapEvent", Toast.LENGTH_SHORT).show();
+        return true;
     }
 
     @Override
     public boolean onDown(MotionEvent e) {
-        vipsmessage.setText("onDown");
+        Toast.makeText(getApplicationContext(), "onDown", Toast.LENGTH_SHORT).show();
         return true;
     }
 
     @Override
     public void onShowPress(MotionEvent e) {
-        vipsmessage.setText("onShowPress");
+        Toast.makeText(getApplicationContext(), "onShowPress", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public boolean onSingleTapUp(MotionEvent e) {
-        vipsmessage.setText("onSingleTapUp");
+        Toast.makeText(getApplicationContext(), "onSingleTapUp", Toast.LENGTH_SHORT).show();
         return true;
 
     }
 
     @Override
     public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        vipsmessage.setText("onScroll");
+        Toast.makeText(getApplicationContext(), "onScroll", Toast.LENGTH_SHORT).show();
         return true;
     }
 
     @Override
     public void onLongPress(MotionEvent e) {
-        vipsmessage.setText("onLongPress");
+        Toast.makeText(getApplicationContext(), "onLongPress", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        vipsmessage.setText("onFling");
+        Toast.makeText(getApplicationContext(), "onFling", Toast.LENGTH_SHORT).show();
         return true;
     }
 }
